538c002fb86167317227aa57974655d8bec3bad2
Javier Luraschi <javierluraschi@hotmail.com>
2020-05-29 14:14:09 -0700

    attempt to improve loading performance with datasets

--- R/alexnet_train.R
+++ R/alexnet_train.R
@@ -10,12 +10,24 @@ alexnet_install <- function() {
 #' to train an image classifier.
 #'
 #' @import keras
+#' @import tfdatasets
 #' @export
 alexnet_train <- function(batch_size = 128,
-                          epochs = 1) {
-
+                          epochs = 2) {
   tiny_imagenet <- pins::pin("http://cs231n.stanford.edu/tiny-imagenet-200.zip")
   tiny_imagenet_train <- gsub("/test.*", "/train", tiny_imagenet[[1]])
+
+  tiny_imagenet_subset <- dir(tiny_imagenet_train, recursive = TRUE, pattern = "*.JPEG", full.names = TRUE)[1:1000]
+  tiny_imagenet_categories <- gsub("^.*/train/|/images.*$", "", tiny_imagenet_subset)
+
+  tiny_imagenet_map <- 0:(length(unique(tiny_imagenet_categories))-1)
+  names(tiny_imagenet_map) <- unique(tiny_imagenet_categories)
+  tiny_imagenet_y <- unname(sapply(tiny_imagenet_categories, function(e) tiny_imagenet_map[e]))
+
+  tiny_imagenet_data <- tibble::tibble(
+    img = tiny_imagenet_subset,
+    cat = to_categorical(tiny_imagenet_y, length(unique(tiny_imagenet_y)))
+  )
 
   model <- keras_model_sequential()
 
@@ -24,31 +36,31 @@ alexnet_train <- function(batch_size = 128,
                   strides = c(4, 4), input_shape = c(224, 224, 3), padding = "valid") %>%
     layer_activation("relu") %>%
     layer_max_pooling_2d(pool_size = c(2, 2), strides = c(2, 2), padding = "valid") %>%
-    
+
     layer_conv_2d(filters = 256, kernel_size = c(5, 5), strides = c(1, 1), padding = "valid") %>%
     layer_activation("relu") %>%
     layer_max_pooling_2d(pool_size = c(2, 2), strides = c(2, 2), padding = "valid") %>%
-    
+
     layer_conv_2d(filters = 384, kernel_size = c(3, 3), strides = c(1, 1), padding = "valid") %>%
     layer_activation("relu") %>%
-    
+
     layer_conv_2d(filters = 384, kernel_size = c(3, 3), strides = c(1, 1), padding = "valid") %>%
     layer_activation("relu") %>%
-    
+
     layer_conv_2d(filters = 256, kernel_size = c(3, 3), strides = c(1, 1), padding = "valid") %>%
     layer_activation("relu") %>%
     layer_max_pooling_2d(pool_size = c(2, 2), strides = c(2, 2), padding = "valid") %>%
-    
+
     layer_flatten() %>%
     layer_dense(4096) %>%
     layer_activation("relu") %>%
     layer_dropout(rate = 0.5) %>%
-    
+
     layer_dense(4096) %>%
     layer_activation("relu") %>%
     layer_dropout(rate = 0.5) %>%
-     
-    layer_dense(200) %>%
+
+    layer_dense(length(unique(tiny_imagenet_data$cat))) %>%
     layer_activation("softmax")
 
   model %>% compile(
@@ -57,18 +69,43 @@ alexnet_train <- function(batch_size = 128,
     metrics = "accuracy"
   )
 
-  datagen <- image_data_generator(
-    width_shift_range = 0.2,
-    height_shift_range = 0.2,
-    horizontal_flip = TRUE,
-    zca_whitening = TRUE
-  )
+  random_bsh <- function(img) {
+    img %>%
+      tf$image$random_brightness(max_delta = 0.3) %>%
+      tf$image$random_contrast(lower = 0.5, upper = 0.7) %>%
+      tf$image$random_saturation(lower = 0.5, upper = 0.7) %>%
+      # make sure we still are between 0 and 1
+      tf$clip_by_value(0, 1)
+  }
 
-  model %>% fit_generator(
-    flow_images_from_directory(directory = tiny_imagenet_train,
-                               batch_size = 128,
-                               generator = datagen,
-                               target_size = c(224, 224)),
-    steps_per_epoch = as.integer(100200 / 128)
-  )
+  create_dataset <- function(data, train, batch_size = 32L) {
+    dataset <- data %>%
+      tensor_slices_dataset() %>%
+      dataset_map(~.x %>% purrr::list_modify(
+        img = tf$image$decode_jpeg(tf$io$read_file(.x$img))
+      )) %>%
+      dataset_map(~.x %>% purrr::list_modify(
+        img = tf$image$convert_image_dtype(.x$img, dtype = tf$float32)
+      )) %>%
+      dataset_map(~.x %>% purrr::list_modify(
+        img = tf$image$resize(.x$img, size = shape(224, 224))
+      ))
+
+    if (train) {
+      dataset <- dataset_map(dataset, ~.x %>% purrr::list_modify(
+        img = random_bsh(.x$img)
+      ))
+    }
+
+    if (train) {
+      dataset_shuffle(dataset, buffer_size = batch_size*128)
+    }
+
+    dataset_batch(dataset, batch_size) %>%
+      dataset_map(unname) # Keras needs an unnamed output.
+  }
+
+  model %>% fit(
+    create_dataset(tiny_imagenet_data, TRUE),
+    epochs = epochs)
 }
